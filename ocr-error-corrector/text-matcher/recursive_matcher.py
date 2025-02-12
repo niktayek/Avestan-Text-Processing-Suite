0ea51f594707785112f802b79f5a93cff975384b
Seyedsoroush Hashemi <hashemi.soroush@gmail.com>
2025-02-12 01:29:52 +0100

    Fix recursive_matcher

--- ocr-error-corrector/text-matcher/recursive_matcher.py
+++ ocr-error-corrector/text-matcher/recursive_matcher.py
@@ -14,7 +14,8 @@ def match():
     ocr_words = load_ocr_words()
 
     end_manual_index, end_ocr_index, matches, _ = recursive_match(manual_words, ocr_words, 0, 0, 0)
-    print(f"Matched OCR: 0 to {end_ocr_index} with manual: 0 to {end_manual_index}")
+    print(f"Matched {len(matches)} words")
+    print(f"OCR: 0 to {end_ocr_index} with manual: 0 to {end_manual_index}")
     with open("res/matches.json", "w") as f:
         f.write(json.dumps(matches))
     return
@@ -32,20 +33,63 @@ def recursive_match(manual_words, ocr_words, manual_index, ocr_index, error_coun
 
     if config.LANGUAGE == 'avestan':
         if manual_words[manual_index][1] in config.AVESTAN_MANUAL_IGNORE_LIST:
-            memo[(manual_index, ocr_index, error_counter)] = recursive_match(manual_words, ocr_words, manual_index+1, ocr_index, error_counter)
+            memo[(manual_index, ocr_index, error_counter)] = recursive_match(manual_words, ocr_words, manual_index + 1, ocr_index, error_counter)
             memo[(manual_index, ocr_index, error_counter)][3] += calculate_error_counter(error_counter)
             return memo[(manual_index, ocr_index, error_counter)]
         if ocr_words[ocr_index][1] in config.AVESTAN_OCR_IGNORE_LIST:
-            memo[(manual_index, ocr_index, error_counter)] = recursive_match(manual_words, ocr_words, manual_index, ocr_index+1, error_counter)
+            memo[(manual_index, ocr_index, error_counter)] = recursive_match(manual_words, ocr_words, manual_index, ocr_index + 1, error_counter)
             memo[(manual_index, ocr_index, error_counter)][3] += calculate_error_counter(error_counter)
             return memo[(manual_index, ocr_index, error_counter)]
 
     best = (manual_index, ocr_index, [], float('inf'))
     if single_match(manual_words[manual_index], ocr_words[ocr_index]):
-        memo_val = recursive_match(manual_words, ocr_words, manual_index+1, ocr_index+1, 0)
-        best = [memo_val[0], memo_val[1], [(manual_index, ocr_index)] + memo_val[2], memo_val[3] + calculate_error_counter(error_counter)]
+        memo_val = recursive_match(manual_words, ocr_words, manual_index + 1, ocr_index + 1, 0)
+        best = [
+            memo_val[0],
+            memo_val[1],
+            [(
+                (manual_index, manual_index + 1),
+                (ocr_index, ocr_index + 1)
+            )] + memo_val[2],
+            memo_val[3] + calculate_error_counter(error_counter),
+        ]
+
+    for i in range(2, config.MERGE_THRESHOLD):
+        if not single_match(manual_words[manual_index], ''.join(ocr_words[ocr_index:ocr_index + i])):
+            continue
+
+        candidate = recursive_match(manual_words, ocr_words, manual_index + 1, ocr_index + i, 0)
+        candidate[3] += calculate_error_counter(error_counter)
+        if (len(candidate[2]) + 1, -candidate[3]) > (len(best[2]), -best[3]):
+            best = [
+                candidate[0],
+                candidate[1],
+                [(
+                    (manual_index, manual_index + 1),
+                    (ocr_index, ocr_index + i),
+                )] + candidate[2],
+                candidate[3],
+            ]
+
+    for i in range(2, config.MERGE_THRESHOLD):
+        if not single_match(''.join(manual_words[manual_index:manual_index + i]), ocr_words[ocr_index]):
+            continue
+
+        candidate = recursive_match(manual_words, ocr_words, manual_index + i, ocr_index + 1, 0)
+        candidate[3] += calculate_error_counter(error_counter)
+        if (len(candidate[2]) + 1, -candidate[3]) > (len(best[2]), -best[3]):
+            best = [
+                candidate[0],
+                candidate[1],
+                [(
+                    (manual_index, manual_index + i),
+                    (ocr_index, ocr_index + 1),
+                )] + candidate[2],
+                candidate[3],
+            ]
+
     for i in range(1, config.SKIP_THRESHOLD):
-        candidate = recursive_match(manual_words, ocr_words, manual_index, ocr_index+i, error_counter+i)
+        candidate = recursive_match(manual_words, ocr_words, manual_index, ocr_index + i, error_counter + i)
         candidate[3] += calculate_error_counter(error_counter)
         if (len(candidate[2]), -candidate[3]) > (len(best[2]), -best[3]):
             best = candidate
@@ -54,6 +98,7 @@ def recursive_match(manual_words, ocr_words, manual_index, ocr_index, error_coun
         candidate[3] += calculate_error_counter(error_counter)
         if (len(candidate[2]), -candidate[3]) > (len(best[2]), -best[3]):
             best = candidate
+
     memo[(manual_index, ocr_index, error_counter)] = best
     return memo[(manual_index, ocr_index, error_counter)]
 
@@ -68,7 +113,7 @@ def single_match(manual_word, ocr_word):
 
     if manual_word == ocr_word:
         return True
-    if nltk.edit_distance(manual_word, ocr_word) <= 1:
+    if nltk.edit_distance(manual_word, ocr_word) <= config.DISTANCE_THRESHOLD:
         return True
     return False
 
