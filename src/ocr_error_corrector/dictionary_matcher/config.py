cf91d625bfbc6268d6f1d1ad6411eaaddff48ab7
nikta <niktaayekrang@gmail.com>
2025-03-05 00:13:53 +0100

    Add config to dictionary matcher

---
+++ src/ocr_error_corrector/dictionary_matcher/config.py
@@ -0,0 +1,7 @@
+LANGUAGE = 'avestan'
+
+MANUAL_FILE_PATH = '../../../data/CAB/static_yasna.xml'
+# OCR_FILE_PATH = '../../../data/escriptorium/0090_first_part_for_error_corrector.txt'
+OCR_FILE_PATH = '../../../data/escriptorium/export_doc47_0093_flip_1_text_20250304224732.txt'
+
+DISTANCE_THRESHOLD = 2
