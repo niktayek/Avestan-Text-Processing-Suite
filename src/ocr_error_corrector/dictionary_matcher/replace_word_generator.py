45944b1560371909e290f07743afa6a82bf5fb1f
Seyedsoroush Hashemi <hashemi.soroush@gmail.com>
2025-03-05 00:42:06 +0100

    Fix dictionary matcher algorithm

--- src/ocr_error_corrector/dictionary_matcher/replace_word_generator.py
+++ src/ocr_error_corrector/dictionary_matcher/replace_word_generator.py
@@ -1,6 +1,8 @@
 import json
 from itertools import combinations
 
+import nltk
+
 
 def main():
     matches = json.load(open('res/matches.json'))
@@ -9,13 +11,13 @@ def main():
         if i % 10:
             print(f"matching word {i}/{len(matches)}: {match['ocr_word']}")
 
-        match['replace_word'] = generate_replace_word(match['ocr_word'], match['manual_word'])
+        match['replace_word'] = generate_replace_word(match['ocr_word'], match['manual_word'], match['distance'])
 
     with open('res/replace_dict.json', mode='w', encoding='utf8') as f:
         f.write(json.dumps(matches, ensure_ascii=False, indent=4))
 
 
-def generate_replace_word(ocr_word, manual_word):
+def generate_replace_word(ocr_word, manual_word, allowed_distance):
     potential_features = {
         'a', 'ą', 'ą̇', 'å', 'ā', 'ae', 'aē', 'o', 'ō', 'ao', 'aō', 'z', 'ž', 'uu',
         'ū', 'ī', 'ii', 'ŋ', 'ŋ́', 'ŋᵛ', 's', 'š', 'š́', 'ṣ', 'ṣ̌', 'mh', 'm̨',
@@ -32,23 +34,55 @@ def generate_replace_word(ocr_word, manual_word):
     ocr_consonant_count = len([part for part in ocr_list if part[1]])
     manual_consonant_count = len([part for part in manual_list if part[1]])
     if ocr_consonant_count == manual_consonant_count:
-        ocr_ind = 0
-        manual_ind = 0
-        while ocr_ind < len(ocr_list):
-            if not ocr_list[ocr_ind][1]:
-                ocr_ind += 1
-                continue
-            if not manual_list[manual_ind][1]:
-                manual_ind += 1
-                continue
-            ocr_list[ocr_ind][0] = manual_list[manual_ind][0]
+        return exact_replace_consonants(ocr_list, manual_list)
+
+    if 0 <= ocr_consonant_count - manual_consonant_count <= allowed_distance:
+        return approximate_replace_consonants(ocr_list, manual_list, allowed_distance)
+    if 0 <= manual_consonant_count - ocr_consonant_count <= allowed_distance:
+        return approximate_replace_consonants(manual_list, ocr_list, allowed_distance)
+    return manual_list
+
+
+def exact_replace_consonants(ocr_list, manual_list):
+    ocr_ind = 0
+    manual_ind = 0
+    while ocr_ind < len(ocr_list):
+        if not ocr_list[ocr_ind][1]:
             ocr_ind += 1
+            continue
+        if not manual_list[manual_ind][1]:
             manual_ind += 1
+            continue
+        ocr_list[ocr_ind][0] = manual_list[manual_ind][0]
+        ocr_ind += 1
+        manual_ind += 1
 
-        return ''.join([part[0] for part in ocr_list])
+    return ''.join([part[0] for part in ocr_list])
 
-    if ocr_consonant_count > manual_consonant_count:
-        ocr_consonant_indices = [i for i, part in enumerate(ocr_list) if part[1]]
+
+def approximate_replace_consonants(editable_list, fix_list, allowed_distance):
+    editable_consonant_count = len([part for part in editable_list if part[1]])
+    fixed_consonant_count = len([part for part in fix_list if part[1]])
+
+    editable_consonant_indices = [i for i, part in enumerate(editable_list) if part[1]]
+    options = []
+    for indices in combinations(editable_consonant_indices, editable_consonant_count - fixed_consonant_count):
+        new_editable_list = editable_list.copy()
+        indices = sorted(indices, reverse=True)
+        for ind in indices:
+            new_editable_list.pop(ind)
+
+        new_editable_word = ''.join([part[0] for part in new_editable_list])
+        fixed_word = ''.join([part[0] for part in fix_list])
+        new_editable_edit_distance = nltk.edit_distance(new_editable_word, fixed_word)
+        if new_editable_edit_distance < allowed_distance:
+            options.append([new_editable_list, new_editable_edit_distance])
+
+    options = sorted(options, key=lambda x: x[1])
+    best_option = options[0]
+    best_editable_list = best_option[0]
+    return exact_replace_consonants(best_editable_list, fix_list)
+
 
 def split_by_consonants(word, consonants):
     ret = []
