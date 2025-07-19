f08c04bdf5a0763b577e497fe43fdf7827d8a1a6
Seyedsoroush Hashemi <hashemi.soroush@gmail.com>
2025-07-19 23:49:32 +0200

    Add utils

--- src/scribal_school_analysis/utils.py
+++ src/scribal_school_analysis/utils.py
@@ -1,3 +1,7 @@
+import csv
+import json
+
+
 def memoize(memoize_for_args: list[str]=None):
     def memoizer(func):
         cache = {}
@@ -8,9 +12,24 @@ def memoize(memoize_for_args: list[str]=None):
             else:
                 key = tuple(kwargs.get(arg) for arg in memoize_for_args)
 
+            key = json.dumps(key, sort_keys=True)
             if key not in cache:
                 cache[key] = func(*args, **kwargs)
             return cache[key]
 
         return memoized
     return memoizer
+
+
+def write_csv(data: list[dict], output_file):
+    fieldnames = data[0].keys() if data else []
+
+    with open(output_file, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+def read_csv(input_file) -> list[dict]:
+    with open(input_file, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        return list(reader)
