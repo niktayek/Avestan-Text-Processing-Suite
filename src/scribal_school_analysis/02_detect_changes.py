e3ac6d0c6ed857d2c2ac5307ea36cd6c26d1a9d0
Seyedsoroush Hashemi <hashemi.soroush@gmail.com>
2025-07-21 16:07:05 +0200

    Split changes into a list

--- src/scribal_school_analysis/02_detect_changes.py
+++ src/scribal_school_analysis/02_detect_changes.py
@@ -38,14 +38,7 @@ def main():
     feature_catalog = pd.read_csv(FEATURE_CATALOG_CSV)
 
     df["changes"] = df.apply(func=detect_changes, axis=1)
-    df["features_comment"] = df["changes"].apply(partial(comment_on_changes, feature_catalog=feature_catalog))
-    df["features_documented"] = df.apply(
-        func=lambda row:
-            True if row["features_comment"] else
-            False if row["changes"] else
-            None,
-        axis=1,
-    )
+    df["changes"] = df["changes"].apply(partial(attach_feature_metadata, feature_catalog=feature_catalog))
 
     df = df[[col for col in df.columns if col != 'address'] + ['address']]
     df.to_csv(OUTPUT_CSV, index=False)
@@ -100,10 +93,10 @@ def dp_changes(reference_tokens: list[str], generated_tokens: list[str]) -> str 
             i -= 1
             j -= 1
         elif i > 0 and (j == 0 or dp[i][j] == dp[i - 1][j] + 1):
-            changes.append({"type": "delete", "from": reference_tokens[i - 1], "to": None})
+            changes.append({"type": "delete", "from": reference_tokens[i - 1]})
             i -= 1
         elif j > 0 and (i == 0 or dp[i][j] == dp[i][j - 1] + 1):
-            changes.append({"type": "insert", "from": None, "to": generated_tokens[j - 1]})
+            changes.append({"type": "insert", "to": generated_tokens[j - 1]})
             j -= 1
         else:
             changes.append({"type": "replace", "from": generated_tokens[j - 1], "to": reference_tokens[i - 1]})
@@ -111,22 +104,50 @@ def dp_changes(reference_tokens: list[str], generated_tokens: list[str]) -> str 
             j -= 1
     changes.reverse()
 
-    changes = [
-        f"{change['from']} to {change['to']}" if change['type'] == "replace" else
-        f"{change['from']} deleted" if change['type'] == "delete" else
-        f"{change['to']} inserted"
-        for change in changes
-    ]
+    for change in changes:
+        change['str'] = (
+            f"{change['to']} for {change['from']}" if change['type'] == 'replace' else
+            f"{change['to']} inserted" if change['type'] == 'insert' else
+            f"{change['from']} deleted"
+        )
 
-    return ", ".join(changes) if changes else None
+    return changes
 
-def comment_on_changes(changes: list[dict[str, str]], feature_catalog: pd.DataFrame) -> dict[str, str] | None:
+def attach_feature_metadata(changes: list[dict[str, str]], feature_catalog: pd.DataFrame) -> dict[str, str] | None:
     if not changes:
-        return None
+        return changes
 
-    for feature in feature_catalog.itertuples(index=False):
-        if re.search(feature.Pattern, changes):
-            return feature.Description
+    for change in changes:
+        change_str = (
+            f"{change['from']} for {change['to']}" if change['type'] == 'replace' else
+            f"{change['to']} inserted" if change['type'] == 'insert' else
+            f"{change['from']} deleted"
+        )
+        change['is_documented'] = False
+        change['description'] = None
+        for feature in feature_catalog.itertuples(index=False):
+            if re.search(feature.Pattern, change_str):
+                change["description"] = feature.Description
+                change["is_documented"] = True
+                break
+    return changes
 
 if __name__ == "__main__":
-    main()
+    manuscript_ids = [
+        "0005",
+        "0006",
+        "0040",
+        "0015",
+        "0060",
+        "0083",
+        "0088",
+        "0400",
+        "0410",
+        "0510",
+    ]
+    for manuscript_id in manuscript_ids:
+        MANUSCRIPT_ID = manuscript_id
+        INPUT_CSV = f"data/CAB/Yasna/{MANUSCRIPT_ID}_matches.csv"
+        OUTPUT_CSV = os.path.join(OUTPUT_DIR, f"{MANUSCRIPT_ID}_matches_with_changes.csv")
+        print(f"Processing manuscript {MANUSCRIPT_ID}...")
+        main()
