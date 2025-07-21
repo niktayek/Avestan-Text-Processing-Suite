c4fdf5638cc2aaf013116c74fe1c9bccdc2c83eb
Seyedsoroush Hashemi <hashemi.soroush@gmail.com>
2025-07-21 16:22:09 +0200

    Fix scribal school assignment data format

--- src/scribal_school_analysis/06_propose_feature_catalog.py
+++ src/scribal_school_analysis/06_propose_feature_catalog.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from collections import defaultdict
 
 from .config import OUTPUT_DIR
 
@@ -10,7 +11,7 @@ QUALITATIVE_FEATURE_CATALOG_PATH = os.path.join(OUTPUT_DIR, "qualitative_feature
 
 def main():
     frequency_matrix = pd.read_csv(FREQUENCY_MATRIX_PATH, index_col='manuscript', dtype={'manuscript': str})
-    scribal_school_assignment = pd.read_csv(SCRIBAL_SCHOOL_ASSIGNMENT_PATH, dtype=str)
+    scribal_school_assignment = read_scribal_school_assignment(SCRIBAL_SCHOOL_ASSIGNMENT_PATH)
 
     quantitative_feature_catalog = create_quantitative_feature_catalog(scribal_school_assignment, frequency_matrix)
     quantitative_feature_catalog.to_csv(QUANTITATIVE_FEATURE_CATALOG_PATH)
@@ -18,19 +19,21 @@ def main():
     qualitative_feature_catalog = create_qualitative_feature_catalog(quantitative_feature_catalog)
     qualitative_feature_catalog.to_csv(QUALITATIVE_FEATURE_CATALOG_PATH)
 
-def create_quantitative_feature_catalog(scribal_school_assignment, frequency_matrix):
+def create_quantitative_feature_catalog(scribal_school_assignment: dict[str, list[str]], frequency_matrix: pd.DataFrame) -> pd.DataFrame:
+    schools = set()
+    for manuscript, schools_list in scribal_school_assignment.items():
+        schools.update(schools_list)
+
     feature_catalog = pd.DataFrame(
-        index=pd.Index(scribal_school_assignment['scribal_school'].unique(), name='scribal_school', dtype=str),
+        index=pd.Index(schools, name='scribal_school', dtype=str),
         columns=frequency_matrix.columns,
         dtype=float,
     )
     feature_catalog = feature_catalog.fillna(0)
 
-    for school in scribal_school_assignment['scribal_school'].unique():
-        manuscripts = scribal_school_assignment[scribal_school_assignment['scribal_school'] == school]['manuscript']
-        for manuscript in manuscripts:
-            if manuscript in frequency_matrix.index:
-                feature_catalog.loc[school] += frequency_matrix.loc[manuscript]
+    for manuscript, scribal_schools in scribal_school_assignment.items():
+        for school in scribal_schools:
+            feature_catalog.loc[school] += frequency_matrix.loc[manuscript]
     feature_catalog = feature_catalog.fillna(0)
     return feature_catalog
 
@@ -53,6 +56,16 @@ def create_qualitative_feature_catalog(quantitative_feature_catalog):
     )
 
     return feature_catalog
+
+def read_scribal_school_assignment(file_path) -> dict[str, list[str]]:
+    scribal_school_assignment = pd.read_csv(SCRIBAL_SCHOOL_ASSIGNMENT_PATH, dtype=str)
+    assignment = defaultdict(list)
+    for _, row in scribal_school_assignment.iterrows():
+        manuscript = row['manuscript']
+        schools = row['scribal_school'].split(',')
+        for school in schools:
+            assignment[manuscript].append(school.strip())
+    return assignment
 
 if __name__ == "__main__":
     main()
