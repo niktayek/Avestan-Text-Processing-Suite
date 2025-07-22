ad31953f3fa264ecabaf7d91eef57b9ddaaa5c17
Seyedsoroush Hashemi <hashemi.soroush@gmail.com>
2025-07-22 22:53:57 +0200

    Rename generated to transliterated

--- src/scribal_school_analysis/01_match_tokens-dictionary.py
+++ src/scribal_school_analysis/01_match_tokens-dictionary.py
@@ -9,6 +9,7 @@ from dataclasses import asdict
 from src.cab.cab_xml import CABXML
 from src.escriptorium.ocr_xml import OCRXML
 
+# INPUT
 REFERENCE_FILES_PATH = [
     # 'data/CAB/static_dron.xml',
     # 'data/CAB/static_videvdad.xml',
@@ -22,9 +23,12 @@ REFERENCE_FILES_PATH = [
 ]
 # TODO: get the matches csv from the following google sheet, the `5,6,15,40,400,60,83,88,510,410_filled` tab
 # https://docs.google.com/spreadsheets/d/1H1EpOKWHuZjCDcr1KdAIpPSTRrSqEt6_sq72NhtGTV8/edit?gid=820015655#gid=820015655
-GENERATED_FILE_PATH = "data/CAB/Yasna/0008_cleaned.xml"
+TRANSLITERATION_FILE_PATH = "data/CAB/Yasna/0008_cleaned.xml"
+
+# OUTPUT
 OUTPUT_FILE_PATH = os.path.join(OUTPUT_DIR, 'matches.csv')
 
+# CONFIGURATION
 DISTANCE_THRESHOLD = 3
 MERGE_THRESHOLD = 3
 ENABLE_NORMALIZER = False
@@ -33,14 +37,14 @@ SORT_BY_DISTANCE = False
 
 def main():
     reference_dictionary = create_dictionary(REFERENCE_FILES_PATH)
-    # generated_words = read_ocr_words(OCR_FILE_PATH)
-    generated_words = read_cab_words(GENERATED_FILE_PATH)
+    # transliterated_words = read_ocr_words(OCR_FILE_PATH)
+    transliterated_words = read_cab_words(TRANSLITERATION_FILE_PATH)
 
     if FOR_TEST:
         # For testing, we use a small set of words
-        generated_words = generated_words[:100]
+        transliterated_words = transliterated_words[:100]
 
-    matches = match_words(generated_words, reference_dictionary)
+    matches = match_words(transliterated_words, reference_dictionary)
     if SORT_BY_DISTANCE:
         matches = sorted(matches, key=lambda x: -x['distance'])
 
@@ -63,24 +67,24 @@ def read_cab_words(file_path):
     cab_words = CABXML(file_path)
     return cab_words
 
-def match_words(generated_words: OCRXML, reference_dictionary: set[str]):
+def match_words(transliterated_words: OCRXML, reference_dictionary: set[str]):
     matches = []
     cur_ind = 0
-    while cur_ind < len(generated_words):
+    while cur_ind < len(transliterated_words):
         if cur_ind % 10:
-            print(f"matching word {cur_ind}/{len(generated_words)}: {generated_words[cur_ind].word}")
+            print(f"matching word {cur_ind}/{len(transliterated_words)}: {transliterated_words[cur_ind].word}")
 
         possible_matches = []
         for i in range(1, MERGE_THRESHOLD + 1):
-            max_ind = min(cur_ind + i, len(generated_words))
-            generated_word = ''.join([generated_words[j].word for j in range(cur_ind, max_ind)])
-            generated_word_address = [generated_words[j].address for j in range(cur_ind, max_ind)]
+            max_ind = min(cur_ind + i, len(transliterated_words))
+            transliterated_word = ''.join([transliterated_words[j].word for j in range(cur_ind, max_ind)])
+            transliterated_word_address = [transliterated_words[j].address for j in range(cur_ind, max_ind)]
             match = find_match(
-                generated_word=generated_word,
+                transliterated_word=transliterated_word,
                 reference_dictionary=reference_dictionary,
             )
             match = copy.deepcopy(match)
-            match['address'] = generated_word_address
+            match['address'] = transliterated_word_address
             possible_matches.append(match)
 
         possible_matches = sorted(possible_matches, key=lambda match: (match['distance'], -len(match["reference"])))
@@ -90,22 +94,22 @@ def match_words(generated_words: OCRXML, reference_dictionary: set[str]):
     return matches
 
 
-@memoize(memoize_for_args=['generated_word'])
-def find_match(generated_word: str, reference_dictionary: set[str]):
-    if generated_word in reference_dictionary:
-        return {'generated': generated_word, 'reference': generated_word, 'distance': 0}
+@memoize(memoize_for_args=['transliterated_word'])
+def find_match(transliterated_word: str, reference_dictionary: set[str]):
+    if transliterated_word in reference_dictionary:
+        return {'transliterated': transliterated_word, 'reference': transliterated_word, 'distance': 0}
 
     matched_words = []
     for reference_word in reference_dictionary:
-        edit_distance = Levenshtein.distance(generated_word, reference_word)\
-            if not ENABLE_NORMALIZER else Levenshtein.distance(normalize(generated_word), normalize(reference_word))
+        edit_distance = Levenshtein.distance(transliterated_word, reference_word)\
+            if not ENABLE_NORMALIZER else Levenshtein.distance(normalize(transliterated_word), normalize(reference_word))
         if edit_distance <= DISTANCE_THRESHOLD:
             matched_words.append({'reference': reference_word, 'distance': edit_distance})
     matched_words = sorted(matched_words, key=lambda x: x['distance'])
     if not matched_words:
-        return {'generated': generated_word, 'reference': '', 'distance': 1000}
+        return {'transliterated': transliterated_word, 'reference': '', 'distance': 1000}
     return {
-        'generated': generated_word,
+        'transliterated': transliterated_word,
         'reference': matched_words[0]['reference'],
         'distance': matched_words[0]['distance'],
     }
